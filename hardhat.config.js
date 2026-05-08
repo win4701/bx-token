e8bafd57fe88b7263d81bce7c67ed6d3a8790dfa
Mr Adam <cfellag1@gmail.com>
2026-05-08 16:25:21 +0100

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,20 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.20",
+
+  networks: {
+    bscTestnet: {
+      url: process.env.BSC_TESTNET_RPC,
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 97,
+    },
+
+    bscMainnet: {
+      url: process.env.BSC_MAINNET_RPC,
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 56,
+    },
+  },
+};
